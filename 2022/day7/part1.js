1047717b4c05ecfdc4c2497151894c2f59c84850
Nikita Kurpas <nikitakurpas@gmail.com>
2022-12-07 15:40:02 +0900

    minor improvement for day 7

--- 2022/day7/part1.js
+++ 2022/day7/part1.js
@@ -1,83 +1,82 @@
 const fs = require("fs");
 const input = fs.readFileSync(__dirname + "/input", "utf8");
 
-Array.prototype.last = function() {
-    return this[this.length - 1]
-};
-const root = { name: '/', type: 'dir', size: 0, nodes: [] }
-let currentPath = [root]
+const root = { name: "/", type: "dir", size: 0, nodes: [] };
+let currentPath = [root];
 
-let lastCommand;
+let command;
+let arg;
 input.split("\n").forEach((line) => {
-  if (line.startsWith("$")) { // command format "$ <command>"
-    lastCommand = line.split(" ")[1];
-    const args = line.split(" ")[2];
-    processCommand(lastCommand, args);
+  if (line.startsWith("$")) {
+    [, command, arg] = line.split(" ");
+    processCommand(command, arg);
   } else {
-    processOutput(lastCommand, line);
+    processOutput(command, line);
   }
 });
-
-// Perform a DFS to find the total size of the all directories
-function compute_sizes(node) {
-    if (node.type === 'file') {
-        return node.size;
-    }
-    let totalSize = 0;
-    node.nodes.forEach(child => {
-        totalSize += compute_sizes(child);
-    });
-    node.size = totalSize;
-    return totalSize;
-}
-compute_sizes(root)
-
-// Find all directories with size <= 100000
-
-function find_big_dirs(node) {
-    let totalSize = 0;
-    if (node.type === 'file') {
-        return 0;
-    }
-    if (node.size <= 100000) {
-        totalSize += node.size;
-    }
-    node.nodes.forEach(child => {
-        totalSize += find_big_dirs(child);
-    });
-    return totalSize
-}
-
-console.log(find_big_dirs(root))
+computeSizes(root);
+console.log(findDirs(root));
 
 function processCommand(command, args) {
   switch (command) {
     case "cd":
       if (args === "/") {
         currentPath = [root];
-      } else if (args === '..') {
+      } else if (args === "..") {
         currentPath.pop();
       } else {
-        const nextDir = currentPath.last().nodes.find(node => node.type === 'dir' && node.name === args);
+        const nextDir = currentPath
+          .peek()
+          .nodes.find((node) => node.type === "dir" && node.name === args);
         currentPath.push(nextDir);
       }
-      break;
-    case "ls":
       break;
   }
 }
 
 function processOutput(command, output) {
-    switch (command) {
-        case "ls":
-            const parts = output.split(" ")
-            if (parts[0] === 'dir') {
-                const childNode = { name: parts[1], type: 'dir', size: 0, nodes: [] }
-                currentPath.last().nodes.push(childNode)
-            } else {
-                const childNode = { name: parts[1], type: 'file', size: parseInt(parts[0]) }
-                currentPath.last().nodes.push(childNode)
-            }
-        break;
-    }
+  switch (command) {
+    case "ls":
+      const parts = output.split(" ");
+      if (parts[0] === "dir") {
+        const childNode = { name: parts[1], type: "dir", size: 0, nodes: [] };
+        currentPath.peek().nodes.push(childNode);
+      } else {
+        const childNode = {
+          name: parts[1],
+          type: "file",
+          size: parseInt(parts[0]),
+        };
+        currentPath.peek().nodes.push(childNode);
+      }
+      break;
+  }
+}
+
+// Perform a DFS to find the total size of the all directories
+function computeSizes(node) {
+  if (node.type === "file") {
+    return node.size;
+  }
+  let totalSize = 0;
+  node.nodes.forEach((child) => {
+    totalSize += computeSizes(child);
+  });
+  node.size = totalSize;
+  return totalSize;
+}
+
+// Find all directories with size <= 100000
+function findDirs(node) {
+  let totalSize = 0;
+  if (node.type === "file") {
+    return 0;
+  }
+  if (node.size <= 100000) {
+    totalSize += node.size;
+  }
+  node.nodes.forEach((child) => {
+    totalSize += findDirs(child);
+  });
+  return totalSize;
 }
--- 2022/day7/part2.js
+++ 2022/day7/part2.js
@@ -1,86 +1,87 @@
 const fs = require("fs");
 const input = fs.readFileSync(__dirname + "/input", "utf8");
 
-Array.prototype.last = function() {
-    return this[this.length - 1]
-};
-const root = { name: '/', type: 'dir', size: 0, nodes: [] }
-let currentPath = [root]
+const root = { name: "/", type: "dir", size: 0, nodes: [] };
+let currentPath = [root];
 
-let lastCommand;
+let command;
+let arg;
 input.split("\n").forEach((line) => {
-  if (line.startsWith("$")) { // command format "$ <command>"
-    lastCommand = line.split(" ")[1];
-    const args = line.split(" ")[2];
-    processCommand(lastCommand, args);
+  if (line.startsWith("$")) {
+    [, command, arg] = line.split(" ");
+    processCommand(command, arg);
   } else {
-    processOutput(lastCommand, line);
+    processOutput(command, line);
   }
 });
-
-// Perform a DFS to find the total size of the all directories
-function compute_sizes(node) {
-    if (node.type === 'file') {
-        return node.size;
-    }
-    let totalSize = 0;
-    node.nodes.forEach(child => {
-        totalSize += compute_sizes(child);
-    });
-    node.size = totalSize;
-    return totalSize;
-}
-compute_sizes(root)
-
-const totalSystemSpace = 70000000;
-const unusedSpace = totalSystemSpace - root.size;
+computeSizes(root);
+const unusedSpace = 70000000 - root.size;
 const spaceNeeded = 30000000 - unusedSpace;
-
-// Find a smallest directory that can be deleted
-function find_smallest_dir(node) {
-    if (node.type === 'file') {
-        return 0;
-    }
-    let smallestDir = node;
-    node.nodes.forEach(child => {
-        const smallestChild = find_smallest_dir(child);
-        if (smallestChild.size >= spaceNeeded && smallestChild.size < smallestDir.size) {
-            smallestDir = smallestChild;
-        }
-    });
-    return smallestDir;
-}
-
-console.log(find_smallest_dir(root))
+console.log(findSmallestDir(root, spaceNeeded).size);
 
 function processCommand(command, args) {
   switch (command) {
     case "cd":
       if (args === "/") {
         currentPath = [root];
-      } else if (args === '..') {
+      } else if (args === "..") {
         currentPath.pop();
       } else {
-        const nextDir = currentPath.last().nodes.find(node => node.type === 'dir' && node.name === args);
+        const nextDir = currentPath
+          .peek()
+          .nodes.find((node) => node.type === "dir" && node.name === args);
         currentPath.push(nextDir);
       }
-      break;
-    case "ls":
       break;
   }
 }
 
 function processOutput(command, output) {
-    switch (command) {
-        case "ls":
-            const parts = output.split(" ")
-            if (parts[0] === 'dir') {
-                const childNode = { name: parts[1], type: 'dir', size: 0, nodes: [] }
-                currentPath.last().nodes.push(childNode)
-            } else {
-                const childNode = { name: parts[1], type: 'file', size: parseInt(parts[0]) }
-                currentPath.last().nodes.push(childNode)
-            }
-        break;
+  switch (command) {
+    case "ls":
+      const parts = output.split(" ");
+      if (parts[0] === "dir") {
+        const childNode = { name: parts[1], type: "dir", size: 0, nodes: [] };
+        currentPath.peek().nodes.push(childNode);
+      } else {
+        const childNode = {
+          name: parts[1],
+          type: "file",
+          size: parseInt(parts[0]),
+        };
+        currentPath.peek().nodes.push(childNode);
+      }
+      break;
+  }
+}
+
+// Perform a DFS to find the total size of the all directories
+function computeSizes(node) {
+  if (node.type === "file") {
+    return node.size;
+  }
+  let totalSize = 0;
+  node.nodes.forEach((child) => {
+    totalSize += computeSizes(child);
+  });
+  node.size = totalSize;
+  return totalSize;
+}
+
+// Find the smallest directory that can be deleted
+function findSmallestDir(node, minSize) {
+  if (node.type === "file") {
+    return 0;
+  }
+  let smallestDir = node;
+  node.nodes.forEach((child) => {
+    const smallestChild = findSmallestDir(child);
+    if (
+      smallestChild.size >= minSize &&
+      smallestChild.size < smallestDir.size
+    ) {
+      smallestDir = smallestChild;
     }
+  });
+  return smallestDir;
 }
